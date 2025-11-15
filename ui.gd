eaec85855cab7abf86e7c18e20ca8ff81c4d927e
José Carvalho <josepedroc25@outlook.com>
2025-11-15 12:11:13 +0000

    added on hover object phrases

--- ui.gd
+++ ui.gd
@@ -4,7 +4,33 @@ extends CanvasLayer
 @onready var shelf: ObjectClass = %shelf
 @onready var carboard_box: ObjectClass = %carboard_box
 @onready var status_label: Label = %status_label
+@onready var status_container: PanelContainer = %status_container
 
 func _ready() -> void:
-	bed.mouse_entered.connect(func():status_label.text = "that's a bed")
+	status_container.scale = Vector2.ZERO
 	
+	bed.mouse_entered.connect(on_object_hover.bind(bed.object_name))
+	bed.mouse_exited.connect(on_object_hover_off)
+	
+	shelf.mouse_entered.connect(on_object_hover.bind(shelf.object_name))
+	shelf.mouse_exited.connect(on_object_hover_off)
+	
+	carboard_box.mouse_entered.connect(on_object_hover.bind(carboard_box.object_name))
+	carboard_box.mouse_exited.connect(on_object_hover_off)
+	
+	
+	
+
+
+func on_object_hover(object_name: String):
+	status_container.show()
+	match object_name:
+		"bed":
+			status_label.text = "that's the big cat's bed"
+		"shelf":
+			status_label.text = "a shelf full of pushable stuff"
+		"cardboard box":
+			status_label.text = "my sweet bed"
+
+func on_object_hover_off():
+	status_container.hide()
