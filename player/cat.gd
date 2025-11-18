0c0e41eca5d53d83ece44aa13d2152a16938bd18
José Carvalho <josepedroc25@outlook.com>
2025-11-18 08:31:21 +0000

    added attack animations and collisions

--- player/cat.gd
+++ player/cat.gd
@@ -12,9 +12,12 @@ extends CharacterBody2D
 @onready var fall_gravity: float = ((-2.0 * jump_height) / (jump_time_to_descend * jump_time_to_descend)) * -1.0
 
 @onready var movement_anim: AnimationPlayer = $movement_anim
-@onready var body: Sprite2D = $body
+@onready var body: AnimatedSprite2D = $body
 @onready var head: Sprite2D = $head
 @onready var jump_buffer_timer: Timer = $jump_buffer_timer
+@onready var attack_collision: CollisionShape2D = $attack_area/attack_collision
+@onready var attack_sweep: AnimatedSprite2D = $attack_sweep
+@onready var attack_anim: AnimationPlayer = $attack_anim
 
 
 
@@ -25,7 +28,7 @@ func _physics_process(delta: float) -> void:
 	velocity.y += get_custom_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("jump"):
 		jump_buffer_timer.start()
 		
 	if is_on_floor() and !jump_buffer_timer.is_stopped():
@@ -33,7 +36,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * move_speed 
 		movement_anim.play("walking")
@@ -42,6 +45,9 @@ func _physics_process(delta: float) -> void:
 		movement_anim.play("idle")
 		
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("attack"):
+		attack()
 	
 	if direction > 0.0:
 		body.flip_h = true
@@ -52,3 +58,7 @@ func _physics_process(delta: float) -> void:
 
 func get_custom_gravity() -> float:
 	return jump_gravity if velocity.y < 0.0 else fall_gravity
+
+
+func attack():
+	attack_anim.play("attack")
