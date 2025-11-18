100719d2dd708ca3836813dd7a291a262981844a
José Carvalho <josepedroc25@outlook.com>
2025-11-18 07:56:19 +0000

    added jump variables for better control

--- player/cat.gd
+++ player/cat.gd
@@ -6,7 +6,10 @@ extends CharacterBody2D
 @export var jump_height: float
 @export var jump_time_to_peak: float
 @export var jump_time_to_descend: float
-const JUMP_VELOCITY = -400.0
+
+@onready var jump_velocity: float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity: float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity: float = ((-2.0 * jump_height) / (jump_time_to_descend * jump_time_to_descend)) * -1.0
 
 @onready var movement_anim: AnimationPlayer = $movement_anim
 @onready var body: Sprite2D = $body
@@ -19,15 +22,14 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	velocity.y += get_custom_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept"):
 		jump_buffer_timer.start()
 		
 	if is_on_floor() and !jump_buffer_timer.is_stopped():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -47,3 +49,6 @@ func _physics_process(delta: float) -> void:
 	elif direction < 0.0:
 		body.flip_h = false
 		head.flip_h = false
+
+func get_custom_gravity() -> float:
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
