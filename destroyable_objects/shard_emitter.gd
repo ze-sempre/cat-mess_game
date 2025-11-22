c40ee51acac80d8ebc1b6e51897f85ef36863f5b
José Carvalho <josepedroc25@outlook.com>
2025-11-22 09:01:14 +0000

    fixed shard emitter

--- destroyable_objects/shard_emitter.gd
+++ destroyable_objects/shard_emitter.gd
@@ -1,15 +1,24 @@
-extends Node2D
-class_name ShardEmitter
+class_name ShardEmitter extends Node2D
+
+## Number of break points.
+@export_range(0, 200) var nbr_of_shards: int = 5
+
+## Prevents slim triangles being created at the sprite edges.
+@export var threshold: float = 10.0 
+
+## Minimum impulse of the shards upon breaking.
+@export var min_impulse: float = 50.0 
+
+## Maximum impulse of the shards upon breaking.
+@export var max_impulse: float = 200.0
+
+## How long the shards live.
+@export var lifetime: float = 5.0
+
+## Whether to display the triangles, for debug purposes.
+@export var display_triangles: bool = false 
 
 const SHARD = preload("res://destroyable_objects/shard.tscn")
-
-@export_range(0, 200) var nbr_of_shards: int = 20 #sets the number of break points
-@export var threshhold: float = 10.0 #prevents slim triangles being created at the sprite edges
-@export var min_impulse: float = 50.0 #impuls of the shards upon breaking
-@export var max_impulse: float = 200.0
-@export var lifetime: float = 5.0 #lifetime of the shards
-@export var display_triangles = false #debugging: display sprite triangulation
-
 
 @onready var delete_timer: Timer = $delete_timer
 
@@ -17,7 +26,8 @@ var triangles = []
 var shards = []
 
 func _ready() -> void:
-	delete_timer.timeout.connect(on_delete_timer_timeout)
+	delete_timer.timeout.connect(_on_DeleteTimer_timeout)
+	
 	
 	if get_parent() is Sprite2D:
 		var _rect = get_parent().get_rect()
@@ -27,26 +37,26 @@ func _ready() -> void:
 		points.append(_rect.position + Vector2(_rect.size.x, 0))
 		points.append(_rect.position + Vector2(0, _rect.size.y))
 		points.append(_rect.end)
-		
+
 		#add random break points
 		for i in nbr_of_shards:
 			var p = _rect.position + Vector2(randi_range(0, _rect.size.x), randi_range(0, _rect.size.y))
 			#move outer points onto rectangle edges
-			if p.x < _rect.position.x + threshhold:
+			if p.x < _rect.position.x + threshold:
 				p.x = _rect.position.x
-			elif p.x > _rect.end.x - threshhold:
+			elif p.x > _rect.end.x - threshold:
 				p.x = _rect.end.x
-			if p.y < _rect.position.y + threshhold:
+			if p.y < _rect.position.y + threshold:
 				p.y = _rect.position.y
-			elif p.y > _rect.position.y - threshhold:
+			elif p.y > _rect.end.y - threshold:
 				p.y = _rect.end.y
 			points.append(p)
-		
+
 		#calculate triangles
 		var delaunay = Geometry2D.triangulate_delaunay(points)
 		for i in range(0, delaunay.size(), 3):
 			triangles.append([points[delaunay[i + 2]], points[delaunay[i + 1]], points[delaunay[i]]])
-			
+
 		#create RigidBody2D shards
 		var texture = get_parent().texture
 		for t in triangles:
@@ -61,7 +71,6 @@ func _ready() -> void:
 			shard.get_node("Polygon2D").texture = texture
 			shard.get_node("Polygon2D").polygon = t
 			shard.get_node("Polygon2D").position = -center
-			shard.get_node("Polygon2D").texture_offset = texture.get_size()/2 
 			shard.gravity_scale = 0
 			#shrink polygon so that the collision shapes don't overlapp
 			var shrunk_triangles = Geometry2D.offset_polygon(t, -2)
@@ -74,10 +83,12 @@ func _ready() -> void:
 		queue_redraw()
 		call_deferred("add_shards")
 
+
 func add_shards() -> void:
 	for s in shards:
 		get_parent().add_child(s)
-		
+
+
 func shatter() -> void:
 	randomize()
 	get_parent().self_modulate.a = 0
@@ -86,13 +97,15 @@ func shatter() -> void:
 		var direction = Vector2.UP.rotated(randf_range(0, 2*PI))
 		var impulse = randf_range(min_impulse, max_impulse)
 		s.apply_central_impulse(direction * impulse)
-		s.get_node("CollisionPolygon2D").set_deferred("disabled" ,false)
+		s.get_node("CollisionPolygon2D").set_deferred("disabled", false)
 		s.show()
 	delete_timer.start(lifetime)
 
-func on_delete_timer_timeout():
+
+func _on_DeleteTimer_timeout() -> void:
 	get_parent().queue_free()
-	
+
+
 func _draw() -> void:
 	if display_triangles:
 		for i in triangles:
