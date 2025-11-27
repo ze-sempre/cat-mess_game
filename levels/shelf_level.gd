ac12aa28ce659fc33b617e5d527fe73ec6b16981
José Carvalho <josepedroc25@outlook.com>
2025-11-27 08:17:45 +0000

    added fall off level detection and level restart

--- levels/shelf_level.gd
+++ levels/shelf_level.gd
@@ -4,3 +4,8 @@ extends Node2D
 
 func _ready() -> void:
 	circle_transition_screen.fade_from_black()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	await circle_transition_screen.fade_to_black()
+	get_tree().reload_current_scene()
